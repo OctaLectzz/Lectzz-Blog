3fa32cfc52b81308600e23385db28bc2d42fa278
unknown <octavyan4@gmail.com>
2023-02-13 23:21:41 +0700

    NEW : Create Post Form

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -1,12 +1,14 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
+
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">My Posts</h1>
 </div>
 
 
 <div class="table-responsive col-lg-8">
+  <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create New Post</a>
     <table class="table table-striped table-sm">
       <thead>
         <tr>
